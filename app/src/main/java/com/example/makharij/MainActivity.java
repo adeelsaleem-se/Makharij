008bc052501c4c2f46ef4dd97d417333ab848f4f
Adeel Saleem <62308816+adeelsaleem-se@users.noreply.github.com>
2021-11-09 22:31:49 +0500

    result_screen.xml created and updated

--- app/src/main/java/com/example/makharij/MainActivity.java
+++ app/src/main/java/com/example/makharij/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.makharij;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,16 +16,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void learning(View view) {
-        Intent fp=new Intent(getApplicationContext(),ResultScreen.class);
+        Intent fp=new Intent(getApplicationContext(),Learning.class);
         startActivity(fp);
     }
 
     public void quiz(View view) {
-        Intent fp=new Intent(getApplicationContext(),ResultScreen.class);
+        Intent fp=new Intent(getApplicationContext(),Quiz.class);
         startActivity(fp);
     }
 
     public void repo(View view) {
-
+        String url = "https://github.com/adeelsaleem-se/Makharij";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/makharij/ResultScreen.java
+++ app/src/main/java/com/example/makharij/ResultScreen.java
@@ -2,13 +2,32 @@ package com.example.makharij;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ResultScreen extends AppCompatActivity {
 
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_screen);
+        txt.findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+
+        txt.setText("Score " + intent.getStringExtra("score") + "out of 5!");
+    }
+
+    public void share(View view) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, txt.getText());
+        sendIntent.setType("text/plain");
+
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
     }
 }
